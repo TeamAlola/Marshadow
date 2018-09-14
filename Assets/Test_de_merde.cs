ce5cc25d5d026c2423dde99abefd58674a45ad2f
Ptitpygme <43262872+Ptitpygme@users.noreply.github.com>
2018-09-15 01:21:43 +0200

    Tower v0.0.1
    Ajout d'une tour qui vise.

---
+++ Assets/Test_de_merde.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_de_merde : MonoBehaviour {
+
+   private RaycastHit2D item;
+    private float timerFire = 0;
+    public GameObject Balle;
+
+	
+	// Update is called once per frame
+	void Update () {
+        timerFire = timerFire + Time.deltaTime;
+        if (timerFire > 3)
+        {
+            item = Physics2D.CircleCast(this.transform.position, 10, Vector2.zero);
+            if (item)
+            {
+                Debug.Log("ENNEMI EN VUE");
+                Debug.Log(item.point);
+                GameObject projectil = Instantiate(Balle, this.transform);
+                projectil.GetComponent<tir>().SetVitesse(new Vector2(item.transform.position.x-this.transform.position.x, 
+                    item.transform.position.y-this.transform.position.y).normalized*150);
+                projectil.GetComponent<tir>().Autre();
+            }
+            else
+            {
+                Debug.Log("JE VOIS RIEN!!!!");
+            }
+            timerFire = 0;
+        }
+	}
+}
---
+++ Assets/tir.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tir : MonoBehaviour {
+
+    Rigidbody2D truc;
+
+    Vector2 vitesse;
+
+	// Use this for initialization
+	public void Autre () {
+        truc = this.GetComponent<Rigidbody2D>();
+        truc.AddForce(vitesse);        
+	}
+	
+    public void SetVitesse(Vector2 vitesseInit) { vitesse = vitesseInit; }
+}
