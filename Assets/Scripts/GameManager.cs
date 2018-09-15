75e0d0568e7381ae1408fc837ce0b0edfc3704b1
Darlysia <darlysia@users.noreply.github.com>
2018-09-15 10:07:28 +0200

    spawn monstre

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,16 +13,37 @@ public class GameManager : MonoBehaviour {
     public List<Tour> toursAchetees;
     public int nbvague;
     public int numerovague;
-
+    public GameObject minion;
+    public GameObject spawn;
     public static GameManager gameManager;
 
-    public void NewVague()
+    //fait apparaitre un minion de la liste sur la map
+    public void Spawn()
+    {
+        Instantiate(minion,spawn.transform.position,spawn.transform.rotation);
+    }
+
+    public void NewVague(int i)
     {
         numerovague++;
-        for(int i = 0; i<30; i ++)
+        for(int j = 0; i<30; i ++)
         {
-            monstres.Add(new Monstre(1, 1, 1));
+            monstres.Add(new Monstre(i, i+1/2, i));
         }
+        float time=0f;
+        float temps=0f;
+
+        time += Time.deltaTime;
+        while (time < monstres.Count)
+        {
+            if (time >= temps)
+            {
+                GameManager.gameManager.Spawn();
+                Debug.Log("Un minion a spawn");
+                temps++;
+            }
+        }
+
     }
 
     public void Gagner()
@@ -35,21 +56,28 @@ public class GameManager : MonoBehaviour {
 
         Debug.Log("Tu pus");
     }
-    // Use this for initialization
-    void Start () {
 
-        if (!gameManager) { gameManager = this; }
+    public void InitGameData()
+    {
         nbvague = 2;
         numerovague = 1;
         joueur = new Joueur(10, 50);
         monstres = new List<Monstre>();
         toursAchetables = new List<Tour>();
-        toursAchetees = new List<Tour>();      
-        monstres.Add(new Monstre(1,1,1));
-        monstres.Add(new Monstre(1, 1, 1));      
-        toursAchetables.Add(new Tour(1, 1));        
+        toursAchetees = new List<Tour>();
+        toursAchetables.Add(new Tour(1, 1));
+    }
 
-	}
+    // Use this for initialization
+    void Start () {
+
+        if (!gameManager) { gameManager = this; }
+
+        InitGameData();
+        //quand timer fini
+        NewVague(numerovague);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
