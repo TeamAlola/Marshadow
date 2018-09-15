313ff0d099e0ffa6db962c8dc8a83bae744ce22a
MagikarpLvl5 <36775948+MagikarpLvl5@users.noreply.github.com>
2018-09-15 12:12:34 +0200

    gamemanager updated

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class GameManager : MonoBehaviour {
 
     public TextMeshProUGUI timer;
     public TextMeshProUGUI pv;
     public TextMeshProUGUI argent;
+    public GameObject achatMenu;
     public Joueur joueur;
     public HUD hud;
     public List<Monstre> monstres;
@@ -45,6 +47,20 @@ public class GameManager : MonoBehaviour {
         }
     }
 
+    public void AcheterTour(int tour)
+    {
+        Tour t = toursAchetables.ElementAtOrDefault(tour);
+        if (joueur.argent >= t.valeur)
+        {
+            joueur.PerdreArgent(t.valeur);
+            toursAchetees.Add(new Tour(t.valeur, t.Degat));
+        }
+        else
+        {
+            Debug.Log("T'es pauvre gros PD");
+        }
+    }
+
     public void Gagner()
     {
         Debug.Log("T'as un gros zizi!");
@@ -61,12 +77,15 @@ public class GameManager : MonoBehaviour {
     void Start () {
 
         if (!gameManager) { gameManager = this; }
+        Tour zero = new Tour(50, 1);
+        Tour one = new Tour(100, 3);
+        Tour two = new Tour(200, 8);
         isspawn = false;
         nbvague = 2;
         numerovague = 1;
         joueur = new Joueur(10, 50);
         monstres = new List<Monstre>();
-        toursAchetables = new List<Tour>();
+        toursAchetables = new List<Tour> { zero, one, two };
         toursAchetees = new List<Tour>();
         toursAchetables.Add(new Tour(1, 1));
         //quand timer fini
